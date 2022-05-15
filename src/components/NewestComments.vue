610347cfd237da46bc37aa6f324dc79e975c87c2
Limecorner <iversonwang40@gmail.com>
2022-05-15 18:33:46 +0800

    上傳

---
+++ src/components/NewestComments.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card">
+    <div class="card-header">最新評論</div>
+    <div class="card-body">
+      <div v-for="comment in comments" :key="comment.id">
+        <h4>
+          <a href="#"> {{ comment.Restaurant.name }} </a>
+        </h4>
+        <p>{{ comment.text }}</p>
+        by
+        <a href="#"> {{ comment.User.name }} </a>
+        {{ comment.User.createdAt | fromNow }}
+        <hr />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+
+export default {
+  filters: {
+    // 沒有datetime怎辦
+    fromNow(datetime) {
+      return moment(datetime).fromNow();
+    },
+  },
+  props: {
+    comments: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
