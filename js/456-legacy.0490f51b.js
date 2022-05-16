440a3f6d16af964364edde9e5fc6f352be784360
Limecorner <iversonwang40@gmail.com>
2022-05-16 11:55:56 +0800

    deploy

---
+++ js/456-legacy.0490f51b.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkforum_front_end_vue"]=self["webpackChunkforum_front_end_vue"]||[]).push([[456],{456:function(n,t,e){e.r(t),e.d(t,{default:function(){return o}});var s=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div",{staticClass:"container py-5"},[e("NavTabs"),e("h1",{staticClass:"mt-5"},[n._v("人氣餐廳")])],1)},u=[],a=e(798),r={components:{NavTabs:a.Z}},l=r,c=e(1001),f=(0,c.Z)(l,s,u,!1,null,null,null),o=f.exports}}]);
+//# sourceMappingURL=456-legacy.0490f51b.js.map
